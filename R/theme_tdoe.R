e0c1798e1d0c38b4b7a873248a55288d1f4357d7
Andrew Marsee <amarsee@live.com>
2019-10-01 10:09:58 -0500

    png call fixed

--- R/theme_tdoe.R
+++ R/theme_tdoe.R
@@ -32,7 +32,11 @@ theme_tdoe <- function(base_size = 8, base_family = "sans") {
 #' @export
 add_tdoe_logo <- function(plot_object, file_path, fig_height = 4.95, fig_width = 9.17, fig_unit = 'in') {
   logo <- magick::image_read("N:/ORP_accountability/projects/Andrew/Crosswalks/logo.png")
-  png(file_path, height = 4.95, width = fig_width, units = fig_unit)
+  if(fig_unit != 'px'){
+    png(file_path, height = fig_height, width = fig_width, units = fig_unit, res = 72)
+  } else {
+    png(file_path, height = fig_height, width = fig_width, units = fig_unit)
+  }
   plot_object
   grid::grid.raster(logo, x = 0.07, y = 0.03, just = c('left', 'bottom'), width = unit(7.5, 'lines'))
   dev.off()
