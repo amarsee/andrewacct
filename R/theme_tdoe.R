87661bad842f3bf701e19dcce06c1fafcf8a9eb9
Andrew Marsee <amarsee@live.com>
2019-10-01 11:13:01 -0500

    fixed logo function

--- R/theme_tdoe.R
+++ R/theme_tdoe.R
@@ -41,7 +41,7 @@ theme_tdoe <- function(base_size = 8, base_family = "sans") {
 add_tdoe_logo <- function(plot_object, file_path, fig_height = 4.95, fig_width = 9.17, fig_unit = 'in', fig_res = 1200) {
   logo <- magick::image_read("N:/ORP_accountability/projects/Andrew/Crosswalks/logo.png")
   png(file_path, height = fig_height, width = fig_width, units = fig_unit, res = fig_res)
-  plot_object
+  print(plot_object)
   grid::grid.raster(logo, x = 0.07, y = 0.03, just = c('left', 'bottom'), width = unit(1.5, 'inches'))
   dev.off()
 }
